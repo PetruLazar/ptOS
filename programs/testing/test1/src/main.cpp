f6244654e5c1e1ecdcd1412bd247ed295a5ecb84
Lazar Petru <adrian_petru72@yahoo.com>
2023-05-18 10:59:11 +0300

    new test program

---
+++ programs/testing/test1/src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream.h>
+#include <syscall.h>
+
+using namespace std;
+
+int main()
+{
+	// char *zero = (char *)0x0000;
+	// char str[64];
+
+	// cin >> str;
+	cout << "Hello world!\n";
+	// cin >> str;
+	// cout << zero;
+
+	return 0;
+}
