71f4cf9155d42357631a1e9b0d6306846cd483a7
Lazar Petru <adrian_petru72@yahoo.com>
2023-07-06 22:56:30 +0300

    changed some runtime settings

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ setlocal
 cd ..
 
 rem ahci interface storage controller
-call ..\qemu\qemu-system-x86_64.exe -monitor stdio -smp 2 -m 1G -cpu Nehalem -device ahci,id=ahci -drive format=raw,file=image.vhd,if=none,id=osimg -device ide-hd,drive=osimg,bus=ahci.0 -drive format=raw,file=bootable/image.vhd,if=none,id=fsimg -device ide-hd,drive=fsimg,bus=ahci.1
+call ..\qemu\qemu-system-x86_64.exe -monitor stdio -smp 2 -m 1G -cpu Nehalem -device ahci,id=ahci -drive format=raw,file=image.vhd,if=none,id=osimg -device ide-hd,drive=osimg,bus=ahci.0 -drive format=raw,file=bootable/imageGPT.vhd,if=none,id=fsimg -device ide-hd,drive=fsimg,bus=ahci.1
 
 rem both ahci controller and ide controller
 rem call ..\qemu\qemu-system-x86_64.exe -monitor stdio -smp 2 -m 1G -cpu Nehalem -device ahci,id=ahci -drive format=raw,file=image.vhd,if=none,id=osimg -device ide-hd,drive=osimg,bus=ahci.0,bootindex=0 -drive format=raw,file=bootable/image.vhd,index=0
