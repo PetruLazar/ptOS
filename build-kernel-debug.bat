0b0f70665227d111f7a0da57ca13c19ecc60a27b
PetruLazar <adrian_petru72@yahoo.com>
2025-12-11 19:32:41 +0200

    separated build scripts into debug and release variants

--- build-kernel-debug.bat
+++ build-kernel-debug.bat
@@ -7,7 +7,7 @@ rem status for building; 0 = success
 set /A kernel = 0
 
 rem compile and assemble kernel
-compileall src/kernel obj/kernel "-I src/libc/include -mcmodel=kernel" || (set /A kernel = 1)
+compileall src/kernel obj/kernel "-I src/libc/include -masm=intel -mcmodel=kernel -D_DEBUG" || (set /A kernel = 1)
 
 rem link kernel
 if %kernel%==0 (linkall obj/kernel bin/kernel.bin "--oformat binary -Ttext 0xFFFFFFFF80010000 -e 0xFFFFFFFF80010000 -T linker.ld -Map bin/ptos.map" src/libc/obj/globals || (set /A kernel = 2))
---
+++ build-kernel-release.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+cd ..
+
+rem status for building; 0 = success
+set /A kernel = 0
+
+rem compile and assemble kernel
+rem compileall src/kernel obj/kernel "-I src/libc/include -mcmodel=kernel -D_DEBUG -gdwarf-4" || (set /A kernel = 1)
+compileall src/kernel obj/kernel "-I src/libc/include -masm=intel -mcmodel=kernel -D_RELEASE -O2" || (set /A kernel = 1)
+
+rem link kernel
+if %kernel%==0 (linkall obj/kernel bin/kernel.bin "--oformat binary -Ttext 0xFFFFFFFF80010000 -e 0xFFFFFFFF80010000 -T linker.ld -Map bin/ptos.map" src/libc/obj/globals || (set /A kernel = 2))
+
+rem install kernel in the filesystem
+if %kernel%==0 (bin-install fs bootable\imageGPT.vhd /ptos/sys/kernel.bin bin/kernel.bin || (set /A kernel = 3))
+
+rem display errors
+if %kernel%==1 (echo "Failed to compile or assemble the kernel!")
+if %kernel%==2 (echo "Failed to link the kernel!")
+if %kernel%==3 (echo "Failed to install the kernel into the disk image!")
+
+rem exit
+if %kernel%==0 (echo "Finished!" & exit /b 0) else (exit /b 1)
